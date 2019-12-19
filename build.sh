7edbfc9c4501149efec9ff088045a62f26c28461
DancingLinks <348368689@qq.com>
2019-12-20 00:08:03 +0800

    finish docker build file

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o go_build ./main
+# Build docker file
+
+mkdir build
+cp -r web build
+cp -r static build
+cp Dockerfile build
+if [ ! -d build/static/server.json  ];then
+  touch build/static/server.json
+fi
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/go_build ./main
+
+cd build
+
+docker build -t dancinglinks/gpu-manager .
